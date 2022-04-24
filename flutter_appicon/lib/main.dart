1f190720de3fc23bd5763dcdfc11d62fd7217aa0
Devid Sumarlie <70418160+tuxcatenate@users.noreply.github.com>
2022-04-24 20:20:18 +0700

    Mobile Programming using flutter
    Devid Sumarlie - 535180008. Berikut adalah repository assignment mobile programming menggunakan flutter

---
+++ flutter_appicon/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Demo'),
+          backgroundColor:
+              Colors.teal, // menambahkan backgorund color untuk appbar
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Image(
+                //image: NetworkImage(
+                //'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg'),
+                image: AssetImage('images/logoftiuntar.png'),
+              ),
+              Text('hello, world'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
